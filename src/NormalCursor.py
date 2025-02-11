43142350dbd87e4afe7eea1cb811447cf84a13c3
Eya Amor <eya.amor@esprit.tn>
2025-02-11 02:03:20 +0100

    fix normal mais reste targets = cibles

--- src/NormalCursor.py
+++ src/NormalCursor.py
@@ -1,32 +1,27 @@
 from Target import Target
-from typing import List
-import math
-from PyQt5.QtGui import QColor
-from PyQt5.QtCore import Qt
 
 class NormalCursor:
-    def __init__(self, targets: List[Target]):
+    def __init__(self, targets):
         self.size: int = 10
         self.x: int = 0
         self.y: int = 0
         self.targets: List[Target] = targets
-        self.toSelect = None  
+        self.toSelect = None
+
+    def paint(self, painter):
+        pass
+
     def move(self, a, b):
         self.x = a
         self.y = b
-        self.cursorInTarget(a, b)
-        
+        self.cursorInTarget(self.x, self.y)
 
     def cursorInTarget(self, x, y):
         self.toSelect = None
         for target in self.targets:
-            target.highlighted = False
             target.selected = False
         for target in self.targets:
             if ((self.x - target.x)**2 + (self.y - target.y)**2)**0.5 <= target.size/2:
-                target.highlighted = True
-                if target.toSelect:
-                    self.toSelect = target
-                    target.selected = True
-                    break
-  
+                self.toSelect = target
+                target.selected = True
+                break
